9a1a954eba421b6bde087fc14782c9c8b0753425
Miles Quive <hilario19931@hotmail.com>
2019-11-16 10:04:10 +0200

    Configuracao do Layout

---
+++ CasaPatraoDesktop/FGeneric.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CasaPatraoDesktop
+{
+    partial class FGeneric
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FGeneric));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.PExitButton = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PGenericContainer = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PExitButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.PExitButton);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.PGenericContainer);
+            this.splitContainer1.Size = new System.Drawing.Size(978, 507);
+            this.splitContainer1.SplitterDistance = 71;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // PExitButton
+            // 
+            this.PExitButton.Image = ((System.Drawing.Image)(resources.GetObject("PExitButton.Image")));
+            this.PExitButton.Location = new System.Drawing.Point(875, 4);
+            this.PExitButton.Name = "PExitButton";
+            this.PExitButton.Size = new System.Drawing.Size(100, 65);
+            this.PExitButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PExitButton.TabIndex = 1;
+            this.PExitButton.TabStop = false;
+            this.PExitButton.Click += new System.EventHandler(this.PExitButton_Click);
+            this.PExitButton.MouseHover += new System.EventHandler(this.PExitButton_MouseHover);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PGenericContainer
+            // 
+            this.PGenericContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PGenericContainer.Location = new System.Drawing.Point(0, 0);
+            this.PGenericContainer.Name = "PGenericContainer";
+            this.PGenericContainer.Size = new System.Drawing.Size(978, 432);
+            this.PGenericContainer.TabIndex = 0;
+            // 
+            // FGeneric
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 507);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FGeneric";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PExitButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox PExitButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel PGenericContainer;
+    }
+}
+
--- CasaPatraoDesktop/Program.cs
+++ CasaPatraoDesktop/Program.cs
@@ -16,7 +16,7 @@ namespace CasaPatraoDesktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FGeneric());
         }
     }
 }
